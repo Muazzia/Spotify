dad6ac0086511000821e141f247a989e23f137af
Muaz <you@example.com>
2023-08-23 21:40:19 +0500

    upadating

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     //   <Main />
     // </div>
     <div className="font-robot grid gap-2 grid-cols-1 sm:grid-cols-[230px,1fr] py-3 mx-3 lg:mx-2 max-w-7xl xl:mx-auto xl:px-2 min-h-screen">
-      <div className="relative">
-        <div className="h-[calc(98vh)] hidden sm:block fixed w-[230px]">
+      <div className="relative hidden sm:block">
+        <div className="h-[calc(98vh)]  fixed w-[230px]">
           <Aside />
         </div>
       </div>
