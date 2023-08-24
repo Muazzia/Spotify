ea2d7c4a3af946d4d2e53a2bc809af1b14492be8
Muaz <you@example.com>
2023-08-24 22:15:28 +0500

    added skeletong

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import useFetchMusic from "../hooks/useFetechMusic";
 import Header from "./Header";
 import MainMusic from "./MainMusic";
+import { Skeleton } from "./ui/skeleton";
 
 const Main = () => {
   const { data, loading } = useFetchMusic();
@@ -15,7 +16,14 @@ const Main = () => {
       </div>
       <div className="p-2 grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-y-4 gap-x-2">
         {loading
-          ? "Loading..."
+          ? Array(5)
+              .fill(1)
+              .map((_, i) => (
+                <Skeleton
+                  key={i}
+                  className="h-[150px] w-[120px] lg:w-[180px]"
+                />
+              ))
           : data.map((music, i) => <MainMusic key={i} data={music} />)}
       </div>
     </main>
